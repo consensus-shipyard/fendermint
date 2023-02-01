371260eacbc633073b82401711d6aef3afe24ce7
Akosh Farkash <aakoshh@gmail.com>
2023-02-01 15:03:30 +0000

    FM-11: Equivalent to ChainMessage

--- fendermint/vm/message/src/lib.rs
+++ fendermint/vm/message/src/lib.rs
@@ -4,6 +4,7 @@ use cid::{multihash, multihash::MultihashDigest, Cid};
 use fvm_ipld_encoding::{to_vec, Error as IpldError, DAG_CBOR};
 use serde::Serialize;
 
+pub mod chain_message;
 pub mod signed_message;
 
 /// Calculate the CID using Blake2b256 digest and DAG_CBOR.
