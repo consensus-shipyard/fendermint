0d9de263a9936d2fe20b7e8b051cd9de2ccf159c
Akosh Farkash <aakoshh@gmail.com>
2023-03-24 09:43:01 +0000

    FM-41: Make multisig state

--- fendermint/vm/actor_interface/src/multisig.rs
+++ fendermint/vm/actor_interface/src/multisig.rs
@@ -3,7 +3,8 @@
 use cid::Cid;
 use fvm_ipld_blockstore::Blockstore;
 use fvm_ipld_encoding::tuple::*;
-use fvm_shared::{address::Address, clock::ChainEpoch, econ::TokenAmount, ActorID};
+use fvm_ipld_hamt::Hamt;
+use fvm_shared::{address::Address, clock::ChainEpoch, econ::TokenAmount, ActorID, HAMT_BIT_WIDTH};
 use serde::{Deserialize, Serialize};
 
 define_code!(MULTISIG { code_id: 9 });
@@ -37,6 +38,16 @@ impl State {
         duration: ChainEpoch,
         balance: TokenAmount,
     ) -> anyhow::Result<Self> {
-        todo!()
+        let empty_map_cid = Hamt::<_, ()>::new_with_bit_width(store, HAMT_BIT_WIDTH).flush()?;
+        let state = Self {
+            signers: signers.into_iter().map(Address::new_id).collect(),
+            num_approvals_threshold: threshold,
+            next_tx_id: Default::default(),
+            initial_balance: balance,
+            start_epoch: start,
+            unlock_duration: duration,
+            pending_txs: empty_map_cid,
+        };
+        Ok(state)
     }
 }
