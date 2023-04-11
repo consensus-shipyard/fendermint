ae475aa60168c9d98cf1e1574d8db65bede29425
Akosh Farkash <aakoshh@gmail.com>
2023-04-11 11:18:55 +0100

    RF: Generic str functions and macro

--- fendermint/vm/encoding/src/lib.rs
+++ fendermint/vm/encoding/src/lib.rs
@@ -4,19 +4,33 @@ use fvm_shared::address::Address;
 use fvm_shared::bigint::BigInt;
 use fvm_shared::econ::TokenAmount;
 use num_traits::Num;
-use serde::de::Error;
+use serde::de::{DeserializeOwned, Error};
 use serde::{de, Deserialize, Serialize, Serializer};
 use serde_with::{DeserializeAs, SerializeAs};
+use std::any::type_name;
+use std::fmt::Display;
 use std::str::FromStr;
 
 use cid::Cid;
 
+/// Serializer which can be used together with the [`serde_with`] crate to annotate
+/// fields that we want to appear as strings in human readable formats like JSON,
+/// and leave as their default serialization formats otherwise (ie. bytes, which
+/// would appear as array of numbers in JSON).
+///
+/// # Example
+///
+/// ```ignore
+/// #[serde_as(as = "Option<IsHumanReadable>")]
+/// pub delegated_address: Option<Address>,
+/// ```
 pub struct IsHumanReadable;
 
-impl SerializeAs<Address> for IsHumanReadable {
-    fn serialize_as<S>(source: &Address, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
+fn serialize_str<T, S>(source: &T, serializer: S) -> Result<S::Ok, S::Error>
+where
+    T: ToString + Serialize,
+    S: Serializer,
+{
     {
         if serializer.is_human_readable() {
             source.to_string().serialize(serializer)
@@ -26,28 +40,56 @@ impl SerializeAs<Address> for IsHumanReadable {
     }
 }
 
-impl<'de> DeserializeAs<'de, Address> for IsHumanReadable {
-    fn deserialize_as<D>(deserializer: D) -> Result<Address, D::Error>
-    where
-        D: de::Deserializer<'de>,
+fn deserialize_str<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+where
+    T: FromStr + DeserializeOwned,
+    <T as FromStr>::Err: Display,
+    D: de::Deserializer<'de>,
+{
     {
         if deserializer.is_human_readable() {
             let s = String::deserialize(deserializer)?;
-            match Address::from_str(&s) {
+            match T::from_str(&s) {
                 Ok(a) => Ok(a),
                 Err(e) => Err(D::Error::custom(format!(
-                    "error deserializing address: {}",
+                    "error deserializing {}: {}",
+                    type_name::<T>(),
                     e
                 ))),
             }
         } else {
-            Address::deserialize(deserializer)
+            T::deserialize(deserializer)
         }
     }
 }
 
+macro_rules! human_readable_str {
+    ($typ: ty) => {
+        impl SerializeAs<$typ> for IsHumanReadable {
+            fn serialize_as<S>(addr: &$typ, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: Serializer,
+            {
+                serialize_str(addr, serializer)
+            }
+        }
+
+        impl<'de> DeserializeAs<'de, $typ> for IsHumanReadable {
+            fn deserialize_as<D>(deserializer: D) -> Result<$typ, D::Error>
+            where
+                D: de::Deserializer<'de>,
+            {
+                deserialize_str(deserializer)
+            }
+        }
+    };
+}
+
+human_readable_str!(Address);
+human_readable_str!(Cid);
+
 impl SerializeAs<TokenAmount> for IsHumanReadable {
-    /// Serialize tokens as human readable string.
+    /// Serialize tokens as human readable decimal string.
     fn serialize_as<S>(tokens: &TokenAmount, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
@@ -77,36 +119,6 @@ impl<'de> DeserializeAs<'de, TokenAmount> for IsHumanReadable {
             }
         } else {
             TokenAmount::deserialize(deserializer)
-        }
-    }
-}
-
-impl SerializeAs<Cid> for IsHumanReadable {
-    /// Serialize tokens as human readable string.
-    fn serialize_as<S>(cid: &Cid, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        if serializer.is_human_readable() {
-            cid.to_string().serialize(serializer)
-        } else {
-            cid.serialize(serializer)
-        }
-    }
-}
-
-impl<'de> DeserializeAs<'de, Cid> for IsHumanReadable {
-    /// Deserialize CID from human readable string.
-    fn deserialize_as<D>(deserializer: D) -> Result<Cid, D::Error>
-    where
-        D: de::Deserializer<'de>,
-    {
-        if deserializer.is_human_readable() {
-            let s = String::deserialize(deserializer)?;
-            Cid::from_str(&s)
-                .map_err(|e| D::Error::custom(format!("error deserializing CID: {}", e)))
-        } else {
-            Cid::deserialize(deserializer)
         }
     }
 }
