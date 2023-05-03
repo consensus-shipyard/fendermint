c305a990c27911e9d1cfaae09f8244baaceaf01e
Akosh Farkash <aakoshh@gmail.com>
2023-05-03 14:36:13 +0100

    FM-95: Create a smoke-test crate

---
+++ fendermint/testing/smoke-test/src/lib.rs
@@ -0,0 +1,10 @@
+//! Run some tests against a pair of Fendermint and Tendermint docker containers running locally.
+//!
+//! Example:
+//!
+//! ```
+//! cd fendermint/testing/smoke-test
+//! cargo make
+//! ```
+//!
+//! Make sure you installed cargo-make by running `cargo install cargo-make` first.
