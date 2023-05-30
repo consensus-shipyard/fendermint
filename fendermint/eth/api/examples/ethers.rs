4dfef01e7b5e0d8a93d047a1ee0086e21a5963e0
Akosh Farkash <aakoshh@gmail.com>
2023-05-30 09:03:42 +0100

    FM-99: Start an ethers example

---
+++ fendermint/eth/api/examples/ethers.rs
@@ -0,0 +1,47 @@
+// Copyright 2022-2023 Protocol Labs
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+//! Example of using the Ethereum JSON-RPC facade with the Ethers provider.
+//!
+//! The example assumes that the following has been started and running in the background:
+//! 1. Fendermint ABCI application
+//! 2. Tendermint Core / Comet BFT
+//! 3. Fendermint Ethereum API facade
+//!
+//! # Usage
+//! ```text
+//! cargo run -p fendermint_eth_api --release --example ethers --
+//! ```
+
+use clap::Parser;
+use tracing::Level;
+
+#[derive(Parser, Debug)]
+pub struct Options {
+    /// The host of the Fendermint Ethereum API endpoint.
+    #[arg(long, default_value = "127.0.0.1", env = "FM_ETH__HTTP__HOST")]
+    pub http_host: String,
+
+    /// The port of the Fendermint Ethereum API endpoint.
+    #[arg(long, default_value = "8545", env = "FM_ETH__HTTP__PORT")]
+    pub http_port: u32,
+
+    /// Enable DEBUG logs.
+    #[arg(long, short)]
+    pub verbose: bool,
+}
+
+impl Options {
+    pub fn log_level(&self) -> Level {
+        if self.verbose {
+            Level::DEBUG
+        } else {
+            Level::INFO
+        }
+    }
+}
+/// See the module docs for how to run.
+#[tokio::main]
+async fn main() {
+    println!("Hello Ethers")
+}
