8fde2dea4e647c3ef62cb0c17d281f1b5e045641
Akosh Farkash <aakoshh@gmail.com>
2023-08-01 16:28:15 +0100

    Update fendermint/vm/actor_interface/src/ipc.rs
    Co-authored-by: adlrocha <6717133+adlrocha@users.noreply.github.com>

--- fendermint/vm/actor_interface/src/ipc.rs
+++ fendermint/vm/actor_interface/src/ipc.rs
@@ -6,7 +6,7 @@
 // Solidity contracts during genesis.
 
 define_id!(GATEWAY { id: 20 });
-define_id!(SUBNETREGISTRY { id: 21 });
+define_id!(SUBNETREGISTRY { id: 65 });
 
 pub mod gateway {
     use ethers::contract::{EthAbiCodec, EthAbiType};
