f08f5d736a9ee81b94c5d2608d1125fab8459ebb
Akosh Farkash <aakoshh@gmail.com>
2023-08-11 15:12:49 +0100

    FM-184: take_until_max_bytes

--- fendermint/abci/src/application.rs
+++ fendermint/abci/src/application.rs
@@ -11,6 +11,8 @@ use tendermint::abci::{request, response, Request, Response};
 use tower::Service;
 use tower_abci::BoxError;
 
+use crate::util::take_until_max_size;
+
 /// Allow returning a result from the methods, so the [`Application`]
 /// implementation doesn't have to be full of `.expect("failed...")`
 /// or `.unwrap()` calls. It is still good practice to use for example
@@ -62,16 +64,8 @@ pub trait Application {
         &self,
         request: request::PrepareProposal,
     ) -> AbciResult<response::PrepareProposal> {
-        let max_tx_bytes: usize = request.max_tx_bytes.try_into().unwrap();
-        let mut size: usize = 0;
-        let mut txs = Vec::new();
-        for tx in request.txs {
-            if size.saturating_add(tx.len()) > max_tx_bytes {
-                break;
-            }
-            size += tx.len();
-            txs.push(tx);
-        }
+        let txs = take_until_max_size(request.txs, request.max_tx_bytes.try_into().unwrap());
+
         Ok(response::PrepareProposal { txs })
     }
 
