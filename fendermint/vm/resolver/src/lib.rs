bbb95af1e2a655f2f3c46d24fa8a650d5ba1356f
Akosh Farkash <aakoshh@gmail.com>
2023-08-18 09:36:34 +0100

    FM-191: Add fendermint_vm_resolver crate

---
+++ fendermint/vm/resolver/src/lib.rs
@@ -0,0 +1,16 @@
+// Copyright 2022-2023 Protocol Labs
+// SPDX-License-Identifier: Apache-2.0, MIT
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
