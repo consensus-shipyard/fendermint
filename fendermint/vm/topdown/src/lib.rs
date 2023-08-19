72943a3ee13e15efce672760cacfd2fd95fbab2f
cryptoAtwill <willes.lau@protocol.ai>
2023-08-19 20:23:49 +0800

    initial

--- fendermint/vm/topdown/src/lib.rs
+++ fendermint/vm/topdown/src/lib.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 mod cache;
+mod sync;
 
 use ipc_sdk::cross::CrossMsg;
 use ipc_sdk::ValidatorSet;
@@ -21,4 +22,33 @@ pub struct IPCParentFinality {
     pub top_down_msgs: Vec<CrossMsg>,
     /// latest validator configuration information from the parent.
     pub validator_set: ValidatorSet,
+}
+
+/// Obtain the latest state information required for IPC from the parent subnet
+pub trait ParentViewProvider {
+    /// Fetch the latest chain head
+    fn latest_height(&self) -> Option<BlockHeight>;
+    /// Fetch the block hash at target height
+    fn block_hash(&self, height: BlockHeight) -> Option<Vec<u8>>;
+    /// Get the top down messages from the nonce of a height
+    fn top_down_msgs(&self, height: BlockHeight, nonce: u64) -> Vec<CrossMsg>;
+    /// Get the latest membership information
+    fn validator_set(&self) -> Option<ValidatorSet>;
+
+    /// Called when finality is committed
+    fn on_finality_committed(&self, finality: &IPCParentFinality);
+}
+
+/// Obtain the latest state information required for IPC from the parent subnet
+#[async_trait]
+pub trait ParentViewQuery {
+    /// Fetch the latest chain head
+    async fn latest_height(&self) -> anyhow::Result<BlockHeight>;
+    /// Fetch the block hash at target height
+    async fn block_hash(&self, height: BlockHeight) -> anyhow::Result<Option<Vec<u8>>>;
+    /// Get the top down messages from the nonce of a height
+    async fn top_down_msgs(&self, height: BlockHeight, nonce: u64)
+        -> anyhow::Result<Vec<CrossMsg>>;
+    /// Get the latest membership information
+    async fn membership(&self) -> anyhow::Result<ValidatorSet>;
 }
---
+++ fendermint/vm/topdown/src/sync.rs
@@ -0,0 +1,3 @@
+// Copyright 2022-2023 Protocol Labs
+// SPDX-License-Identifier: Apache-2.0, MIT
+//! Parent view syncer for top down checkpoint
