ca590c936384b9e39d5c174e582720b0987d2679
cryptoAtwill <willes.lau@protocol.ai>
2023-08-20 20:18:54 +0800

    add update parent view

--- fendermint/vm/topdown/src/sync.rs
+++ fendermint/vm/topdown/src/sync.rs
@@ -11,6 +11,7 @@ use std::cmp::max;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::Duration;
+use crate::error::Error;
 
 /// Constantly syncing with parent through polling
 pub struct PollingParentSyncer<T> {
@@ -64,77 +65,43 @@ async fn sync_with_parent<T: ParentFinalityProvider + Send + Sync + 'static>(
     loop {
         interval.tick().await;
 
-        let
-        let hashes = fetch_block_hashes(&config, &agent_proxy, &lock).await?;
-        update_top_down_msgs(&config, &agent_proxy, &lock).await?;
-        update_membership(&config, &agent_proxy, &lock).await?;
+        let latest_height = match agent_proxy.get_chain_head_height().await {
+            Ok(h) => h,
+            Err(e) => {
+                tracing::warn!("cannot fetch parent chain head due to {e}");
 
-        let mut cache = lock.write().unwrap();
-        for r in hashes {
-            cache.block_hash.insert_after_lower_bound(r.0, r.1);
+                // not throw errors, caller will retry
+                continue;
+            }
+        };
+
+        if latest_height < config.chain_head_delay {
+            tracing::debug!("latest height not more than the chain head delay");
+            continue;
         }
-    }
-}
 
-async fn update_top_down_msgs(
-    _config: &Config,
-    _agent_proxy: &Arc<AgentProxy>,
-    _lock: &LockedCache,
-) -> anyhow::Result<Vec<CrossMsg>> {
-    Ok(vec![])
-}
+        let starting_height = if let Some(h) = provider.latest_height().await {
+            h + 1
+        } else {
+            // if latest_recorded_height is None, it means we have not started fetching any heights
+            // we just use the latest height minus a lower bound as the parent view
+            max(1, latest_height.saturating_sub(config.chain_head_lower_bound))
+        };
 
-async fn update_membership(
-    _config: &Config,
-    _agent_proxy: &Arc<AgentProxy>,
-    _lock: &LockedCache,
-) -> anyhow::Result<ValidatorSet> {
-    Ok(ValidatorSet::default())
-}
+        for h in starting_height..=latest_height {
+            let block_hash = agent_proxy.get_block_hash(h).await?;
+            let validator_set = agent_proxy.get_validator_set(h).await?;
 
-async fn fetch_block_hashes(
-    config: &Config,
-    agent_proxy: &Arc<AgentProxy>,
-    lock: &LockedCache,
-) -> anyhow::Result<Vec<(BlockHeight, Vec<u8>)>> {
-    let latest_height = match agent_proxy.get_chain_head_height().await {
-        Ok(h) => h,
-        Err(e) => {
-            tracing::warn!("cannot fetch parent chain head due to {e}");
-
-            // not throw errors, caller will retry
-            return Ok(vec![]);
+            match provider.new_parent_view(Some((h, block_hash, validator_set)), vec![]).await {
+                Ok(_) => {}
+                Err(e) => match e {
+                    Error::ParentReorgDetected(_) => { todo!() }
+                    _ => unreachable!()
+                }
+            }
         }
-    };
 
-    if latest_height < config.chain_head_delay {
-        tracing::debug!("latest height not more than the chain head delay");
-        return Ok(vec![]);
     }
-
-    let starting_height = {
-        let cache = lock.read().unwrap();
-        // if cache.latest_height() is None, it means we have not started fetching any heights
-        // we just use the latest height minus a lower bound as the parent view
-        let starting_height = cache.block_hash.upper_bound().unwrap_or(max(
-            1,
-            latest_height.saturating_sub(config.chain_head_lower_bound),
-        ));
-        tracing::debug!("polling parent from {starting_height} till {latest_height}");
-
-        starting_height
-
-        // ensure cache is dropped
-    };
-
-    // FIXME: make the fetching batching and concurrent
-    let mut results = vec![];
-    for h in starting_height..=latest_height {
-        let block_hash = agent_proxy.get_block_hash(h).await?;
-        results.push((h, block_hash));
-    }
-
-    Ok(results)
 }
 
 struct IPCAgentProxy {}
@@ -156,7 +123,7 @@ impl IPCAgentProxy {
         todo!()
     }
 
-    pub async fn get_validator_set(&self) -> anyhow::Result<ValidatorSet> {
+    pub async fn get_validator_set(&self, _height: BlockHeight) -> anyhow::Result<ValidatorSet> {
         todo!()
     }
 }
