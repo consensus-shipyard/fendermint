ff3ae6abdcb7ce3bdd5737f3a4012e3634a69950
Akosh Farkash <aakoshh@gmail.com>
2023-10-04 15:39:56 +0100

    TEST: Create a child genesis

--- fendermint/testing/contract-test/tests/staking.rs
+++ fendermint/testing/contract-test/tests/staking.rs
@@ -16,7 +16,7 @@ use std::collections::HashMap;
 
 use arbitrary::{Arbitrary, Unstructured};
 use fendermint_crypto::{PublicKey, SecretKey};
-use fendermint_testing::arb::{ArbSubnetID, ArbTokenAmount};
+use fendermint_testing::arb::{ArbSubnetAddress, ArbSubnetID, ArbTokenAmount};
 use fendermint_testing::{smt::StateMachine, state_machine_test};
 use fendermint_vm_core::Timestamp;
 use fendermint_vm_genesis::ipc::{GatewayParams, IpcParams};
@@ -30,6 +30,7 @@ use fendermint_vm_interpreter::fvm::{
 use fvm_shared::address::Address;
 use fvm_shared::bigint::BigInt;
 use fvm_shared::{econ::TokenAmount, version::NetworkVersion};
+use ipc_sdk::subnet_id::SubnetID;
 use rand::rngs::StdRng;
 use rand::SeedableRng;
 
@@ -44,23 +45,32 @@ struct StakingSystem {
 /// Reference implementation for staking.
 #[derive(Debug, Clone)]
 struct StakingState {
-    /// The parent genesis should include a bunch of accounts we can use to join a subnet.
-    parent_genesis: Genesis,
     /// Accounts with secret key of accounts in case the contract wants to validate signatures.
     accounts: HashMap<Address, StakingAccount>,
+    /// The parent genesis should include a bunch of accounts we can use to join a subnet.
+    parent_genesis: Genesis,
+    /// The child genesis describes the initial validator set to join the subnet
+    child_genesis: Genesis,
+    /// Currently active child validator set.
     child_validators: HashMap<Address, Power>,
 }
 
 impl StakingState {
-    fn new(
-        parent_genesis: Genesis,
-        accounts: Vec<StakingAccount>,
-        child_validators: Vec<(Address, Power)>,
-    ) -> Self {
+    fn new(accounts: Vec<StakingAccount>, parent_genesis: Genesis, child_genesis: Genesis) -> Self {
+        let child_validators = child_genesis
+            .validators
+            .iter()
+            .map(|v| {
+                let addr = Address::new_secp256k1(&v.public_key.0.serialize()).unwrap();
+                (addr, v.power)
+            })
+            .collect();
+
         Self {
-            parent_genesis,
             accounts: accounts.into_iter().map(|a| (a.addr, a)).collect(),
-            child_validators: child_validators.into_iter().collect(),
+            parent_genesis,
+            child_genesis,
+            child_validators,
         }
     }
 }
@@ -126,22 +136,7 @@ impl arbitrary::Arbitrary<'_> for StakingState {
             power: Power(1),
         }];
 
-        // Select some of the accounts to be the initial *child subnet* validators.
-        let child_validators = accounts
-            .iter()
-            .take(num_validators)
-            .map(|a| {
-                // Power has to be a u64.
-                let p = ArbTokenAmount::arbitrary(u)?.0.atto() % BigInt::from(u64::MAX);
-                // Cannot have more than the balance.
-                let p = p.max(a.initial_balance.atto().clone()).min(BigInt::from(1));
-                let p = Power(p.try_into().expect("power must be u64"));
-                Ok((a.addr, p))
-            })
-            .collect::<Result<Vec<_>, _>>()?;
-
-        // Make sure IPC is enabled.
-        let ipc = IpcParams {
+        let parent_ipc = IpcParams {
             gateway: GatewayParams {
                 subnet_id: ArbSubnetID::arbitrary(u)?.0,
                 bottom_up_check_period: 1 + u.choose_index(100)? as u64,
@@ -154,17 +149,63 @@ impl arbitrary::Arbitrary<'_> for StakingState {
             },
         };
 
-        let g = Genesis {
+        let child_subnet_id = SubnetID::new_from_parent(
+            &parent_ipc.gateway.subnet_id,
+            ArbSubnetAddress::arbitrary(u)?.0,
+        );
+
+        let parent_genesis = Genesis {
             chain_name: String::arbitrary(u)?,
             timestamp: Timestamp(u64::arbitrary(u)?),
             network_version: NetworkVersion::V20,
             base_fee: ArbTokenAmount::arbitrary(u)?.0,
             validators: parent_validators,
             accounts: parent_actors,
-            ipc: Some(ipc),
+            ipc: Some(parent_ipc),
         };
 
-        Ok(StakingState::new(g, accounts, child_validators))
+        // Select some of the accounts to be the initial *child subnet* validators.
+        let child_validators = accounts
+            .iter()
+            .take(num_validators)
+            .map(|a| {
+                // Power has to be a u64.
+                let p = ArbTokenAmount::arbitrary(u)?.0.atto() % BigInt::from(u64::MAX);
+                // Cannot have more than the balance.
+                let p = p.max(a.initial_balance.atto().clone()).min(BigInt::from(1));
+                let p = Power(p.try_into().expect("power must be u64"));
+                let v = Validator {
+                    public_key: ValidatorKey(a.public_key),
+                    power: p,
+                };
+                Ok(v)
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
+        let child_ipc = IpcParams {
+            gateway: GatewayParams {
+                subnet_id: child_subnet_id,
+                bottom_up_check_period: 1 + u.choose_index(100)? as u64,
+                top_down_check_period: 1 + u.choose_index(100)? as u64,
+                msg_fee: ArbTokenAmount::arbitrary(u)?.0,
+                majority_percentage: 51 + u8::arbitrary(u)? % 50,
+                min_collateral: ArbTokenAmount::arbitrary(u)?
+                    .0
+                    .max(TokenAmount::from_atto(1)),
+            },
+        };
+
+        let child_genesis = Genesis {
+            chain_name: String::arbitrary(u)?,
+            timestamp: Timestamp(u64::arbitrary(u)?),
+            network_version: NetworkVersion::V20,
+            base_fee: ArbTokenAmount::arbitrary(u)?.0,
+            validators: child_validators,
+            accounts: Vec::new(),
+            ipc: Some(child_ipc),
+        };
+
+        Ok(StakingState::new(accounts, parent_genesis, child_genesis))
     }
 }
 
@@ -192,6 +233,10 @@ impl StateMachine for StakingMachine {
 
         let gateway = GatewayCaller::default();
 
+        // TODO: Call the methods on the gateway to establish the subnet based on `state.child_genesis`:
+        // * Create the subnet with the given ID
+        // * Make all the validators join the subnet by putting down collateral according to their power
+
         StakingSystem {
             parent_state,
             gateway,
@@ -210,9 +255,7 @@ impl StateMachine for StakingMachine {
         todo!()
     }
 
-    fn check_result(&self, cmd: &Self::Command, pre_state: &Self::State, result: &Self::Result) {
-        todo!()
-    }
+    fn check_result(&self, cmd: &Self::Command, pre_state: &Self::State, result: &Self::Result) {}
 
     fn next_state(&self, cmd: &Self::Command, state: Self::State) -> Self::State {
         todo!()
@@ -224,7 +267,6 @@ impl StateMachine for StakingMachine {
         post_state: &Self::State,
         post_system: &Self::System,
     ) {
-        todo!()
     }
 }
 
