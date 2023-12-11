36a920757a8360313afe1f1315c7d7278002dbbc
Akosh Farkash <aakoshh@gmail.com>
2023-12-11 14:56:51 +0000

    FM-363: Rename fullnode.env to node-1.env and make reusable tasks

--- fendermint/testing/snapshot-test/src/lib.rs
+++ fendermint/testing/snapshot-test/src/lib.rs
@@ -1,8 +1,13 @@
 // Copyright 2022-2023 Protocol Labs
 // SPDX-License-Identifier: Apache-2.0, MIT
-//! Run tests against multiple Fendermint+CometBFT docker container pairs locally,
-//! where one is allowed to run for a while and export some snapshots, then another
-//! is started to sync its state directly with it.
+//! Run tests against multiple Fendermint+CometBFT docker container pairs locally:
+//! 0. The default `snapshot-fendermint` and `snapshot-cometbft` pair
+//! 1. A `snapshot-fendermint-1` and `snapshot-cometbft-1` using `scripts/node-1.env`,
+//!    which sync with the default node from genesis on a block-by-block basis.
+//! 2. A `snapshot-fendermint-2` and `snapshot-cometbft-2` using `scripts/node-2.env`,
+//!    which syncs with `node-0` and `node-1` using snapshots (a.k.a. state sync).
+//!
+//! Note that CometBFT state sync requires 2 RPC servers, which is why we need 3 nodes.
 //!
 //! Example:
 //!
