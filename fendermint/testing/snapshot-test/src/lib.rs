acfaa7c4fcd591b43249c5f398e6e2538bd82cbd
Akosh Farkash <aakoshh@gmail.com>
2023-12-12 12:19:56 +0000

    FM-363: Try a diamond setup

--- fendermint/testing/snapshot-test/src/lib.rs
+++ fendermint/testing/snapshot-test/src/lib.rs
@@ -2,10 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 //! Run tests against multiple Fendermint+CometBFT docker container pairs locally:
 //! 0. The default `snapshot-fendermint` and `snapshot-cometbft` pair
-//! 1. A `snapshot-fendermint-1` and `snapshot-cometbft-1` using `scripts/node-1.env`,
-//!    which sync with the default node from genesis on a block-by-block basis.
-//! 2. A `snapshot-fendermint-2` and `snapshot-cometbft-2` using `scripts/node-2.env`,
-//!    which syncs with `node-0` and `node-1` using snapshots (a.k.a. state sync).
+//! 1. A `snapshot-cometbft-1` and `snapshot-cometbft-2`, using `scripts/node-1.env` and `node-2`.env,
+//!    syncing with the default node from genesis on a block-by-block basis, and clear out their history
+//!    to force others who sync with them to use snapshots.
+//! 2. A `snapshot-cometbft-3` using `scripts/node-3.env`,
+//!    which syncs with `node-1` and `node-2` using snapshots (a.k.a. state sync).
 //!
 //! Note that CometBFT state sync requires 2 RPC servers, which is why we need 3 nodes.
 //!
